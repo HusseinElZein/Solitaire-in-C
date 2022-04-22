cf8556f952a2c58265ef6a13e123e8304f18ca97
Yusuf Kara <yusufkarasan@hotmail.com>
2022-04-22 16:49:36 +0200

    Added a pile node. Attempted to print out

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
-typedef struct Node Card;
+typedef struct CardNode Card;
+typedef struct PileNode Pile;
 
-struct Node {
+struct CardNode {
     Card *next;
     Card *prev;
     char number;
     char suit;
 };
 
+struct PileNode {
+    Card *head;
+    Card *tail;
+    Pile *next;
+    Pile *prev;
+    int sizeOfPile;
+};
+
+// Initializing all the columns and foundations to contain NULL.
 Card *C1 = NULL;
 Card *C2 = NULL;
 Card *C3 = NULL;
@@ -22,7 +32,31 @@ Card *F2 = NULL;
 Card *F3 = NULL;
 Card *F4 = NULL;
 
-int addCard(Card *newCard, Card **list){
+int addCard(Card *newCard, Card **list);
+void printBoard(Card *C1, Card *C2, Card *C3, Card *C4, Card *C5, Card *C6, Card *C7, Card *F1, Card *F2, Card *F3, Card *F4);
+void distributeCards();
+
+int main() {
+    Card card = {NULL,NULL,'2','H'};
+    Card card1 = {NULL,NULL,'3','K'};
+    Card card2 = {NULL, NULL, '4', 'P'};
+
+
+    addCard(&card, &C1);
+    addCard(&card1, &C1);
+    addCard(&card2, &C1);
+
+    printBoard(C1, C2, C3, C4, C5, C6, C7, F1, F2, F3, F4);
+
+    printf("%c", C1->suit);
+    printf("\n" "%c", C1->next->suit);
+    printf("\n" "%c", C1->next->next->suit);
+
+    return 0;
+}
+
+
+int addCard(Card *newCard, Card **list) {
     Card *current = (Card *) list;
 
     if (*list == NULL) {
@@ -42,22 +76,20 @@ int addCard(Card *newCard, Card **list){
     return 0;
 }
 
+void printBoard(Card *C1, Card *C2, Card *C3, Card *C4, Card *C5, Card *C6, Card *C7, Card *F1, Card *F2, Card *F3, Card *F4) {
+    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
 
-int main() {
-    Card card = {NULL,NULL,'2','H'};
-    Card card1 = {NULL,NULL,'3','K'};
-    Card card2 = {NULL, NULL, '4', 'P'};
+    // Pointers for the piles, foundations and card nodes:
 
 
-    addCard(&card, &C1);
-    addCard(&card1, &C1);
-    addCard(&card2, &C1);
+}
 
-    printf("%c", C1->suit);
-    printf("\n" "%c", C1->next->suit);
-    printf("\n" "%c", C1->next->next->suit);
+/*
+ * This function is used to separate a pile of cards onto the board by splitting the deck into 7 piles
+ * representing columns.
+ */
+void distributeCards() {
 
-    return 0;
 }
 
 
